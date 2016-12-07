055b7e175e2c38194f43439b39946d0627d40b81
Mo'men Basel <momenbasel@gmail.com>
2016-12-07 21:24:57 +0200

    v1.0

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+error_reporting(0);
+$access_token     = "your_access_token";
+$verify_token     = "meow_meow_meow";
+$hub_verify_token = null;
+
+if (isset($_REQUEST['hub_challenge'])) {
+    $challenge        = $_REQUEST['hub_challenge'];
+    $hub_verify_token = $_REQUEST['hub_verify_token'];
+}
+
+if ($hub_verify_token === $verify_token) {
+    echo $challenge;
+}
+
+$input            = json_decode(file_get_contents('php://input'), true);
+$sender           = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message          = $input['entry'][0]['messaging'][0]['message']['text'];
+$message_to_reply = '';
+
+if (preg_match('[help|Help]', strtolower($message))) {
+    $help_me = "This App can send you photos depends on what you type using Google Custom Search just type what you want to search about eg. jesus  there is 100 search/day only, Have a question? https://goo.gl/OLP4dz";
+    
+    $message_to_reply = $help_me;
+    
+    $jsonData = '{
+            "recipient":{
+                "id":"' . $sender . '"
+            },
+            "message":{
+                "text":"' . $message_to_reply . '"
+            }
+    }';
+} else {
+    $key = "your_key";
+    
+    // your googleapis key https://console.developers.google.com/apis/credentials?project=[your project]
+    
+    $cx = "your cx id";
+    
+    // your cx id https://cse.google.com/cse/manage/all
+    
+    $json_url = "https://www.googleapis.com/customsearch/v1?q=" . $message . "&key=" . $key . "&cx=" . $cx . "&searchType=image";
+    $str      = file_get_contents($json_url);
+    $json     = json_decode($str, true);
+}
+
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' . $access_token;
+$ch  = curl_init($url);
+
+// The message that will be sent
+
+foreach ($json['items'] as &$value) {
+    $links            = $value['link'];
+    $message_to_reply = $links;
+    $jsonData         = '{
+    "recipient":{
+        "id":"' . $sender . '"
+    },
+    "message":{
+
+     "attachment":
+          {
+          "type":"image",
+           "payload":
+             {
+                "url":"' . $message_to_reply . '"
+             }
+          }
+    }
+}';
+    $jsonDataEncoded  = $jsonData;
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json'
+    ));
+    if (!empty($input['entry'][0]['messaging'][0]['message'])) {
+        $result = curl_exec($ch);
+    }
+    
+}
+
+$jsonDataEncoded = $jsonData;
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json'
+));
+if (!empty($input['entry'][0]['messaging'][0]['message'])) {
+    $result = curl_exec($ch);
+}
+
+echo "Hello, I am a chatbot."; // get request on /index.php
+
---
+++ readme.md
@@ -0,0 +1,49 @@
+
+This is a bot for sending photos to your messenger alternative to searching the web for a photo of a specific word.
+
+Why should I use it:- 
+
+1- the image is compressed , so this won't use a lot of your phone data
+(facebook decrease photo quality)
+
+
+2- it's fast , easy just type a word and it will search google images for it
+
+
+3- it can send 100 picture per day
+
+
+
+4- it's good for seeing nudes ;-)
+
+
+sample ready to use one : https://goo.gl/yimg1f
+
+
+
+Requirement :-
+Google custom search 
+Facebook app
+Facebook page
+
+Steps to deploy :-
+
+
+1-  Create a Facebook App(developers.facebook.com/apps) and Page
+
+2- Go to the App Dashboard and under Product Settings click "Add Product" and select "Messenger."
+
+3- Install the Heroku toolbelt from https://toolbelt.heroku.com to launch ,Sign up for free at https://www.heroku.com if you don't have an account yet.
+
+4- Inside the app go to Messenger , then click Setup Webhook, put your heroku app url on Callback, and on Verify Token put meow_meow_meow
+
+5- Scroll up to "Token Generation" , Select a page  and get the page token and save it somewhere.
+
+6- Go to terminal  :
+curl -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token="your_page_token"
+
+7- Edit  $access_token     = "your_access_token";    with your page token
+
+8- Edit the cx and key var with your own cx and key.
+
+9-  git push heroku master 
